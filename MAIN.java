323f161ea63be469a18b6fa70b335d15a344052b
mileagaabriel-dam1 <mileagaabriel@alumnes.ilerna.com>
2026-04-08 19:08:46 +0200

    MAIN pasado a nuevo repositorio

---
+++ MAIN.java
@@ -0,0 +1,13 @@
+package src;
+
+import Vista.VistaJavaFX;
+import javafx.application.Application;
+
+public class MAIN {
+    public static void main(String[] args) {
+        System.out.println(" JOC DEL PINGÜ - VERSIÓN GRÁFICA ");
+        System.out.println("     PINGU-G09-DAM1 - JavaFX     ");
+        
+        Application.launch(VistaJavaFX.class, args);
+    }
+}
